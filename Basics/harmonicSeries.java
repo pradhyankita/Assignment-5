f7c0a183bb1e63a9a4b75dffa17ed82b8ff899d3
pradhyankita <104745890+pradhyankita@users.noreply.github.com>
2022-07-09 10:41:36 +0530

    Add files via upload

---
+++ Basics/harmonicSeries.java
@@ -0,0 +1,20 @@
+package com.BridgeLabz.Basics;
+
+public class harmonicSeries {
+
+        public static void main(String args[])
+        {
+            int num = 5;
+            double result = 0.0;
+
+            System.out.println("The harmonic series is: ");
+
+            while (num > 0) {
+
+                result = result + (double)1 / num;
+                num--;
+                System.out.print(result + ", ");
+            }
+        }
+    }
+
