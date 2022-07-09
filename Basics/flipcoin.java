69208cceededd50910dc001130189aa0360155d3
pradhyankita <104745890+pradhyankita@users.noreply.github.com>
2022-07-09 10:35:00 +0530

    Update flipcoin.java

--- Basics/flipcoin.java
+++ Basics/flipcoin.java
@@ -1,5 +1,45 @@
-package com.BridgeLabz.Basics.Assignment5.Basics;
+package com.BridgeLabz.Basics;
+import java.util.Scanner;
 
-public class flipcoin {
+public class flip
+{
+    public static void main(String[] args)
+    {
+        int heads = 0;
+        int tails = 0;
+        int counter = 1;
+        double randNum = 0.0;
+        Scanner in = new Scanner(System.in);
 
+        System.out.println("How many times will the coin be flipped? ");
+        int flips = in.nextInt();
+        if (flips>0){
+        System.out.println();
+    }
+            else{
+        System.out.println("The number entered is negative, pls enter a positive integer.");
+    }
+
+        while (counter <= flips) {
+            randNum = Math.random();
+            System.out.print(counter + "\t" + randNum);
+
+            if (randNum < .5) {
+                tails++;
+                System.out.println("\t tails");
+            } else {
+                heads++;
+                System.out.println("\t heads");
+            }
+            counter++;
+        }
+        System.out.println();
+        System.out.println("Number of Heads = " + heads);
+        System.out.println("Number of Tails = " + tails);
+
+
+
+    }
 }
+
+
